0131760fc9986d95f8619fe9ac8e2de19553aaa2
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-05-09 16:43:34 -0500

    Created Data and Game Manager

---
+++ CountryFlagGame2/Models/Data.swift
@@ -0,0 +1,32 @@
+//
+//  Data.swift
+//  CountryFlagGame2
+//
+//  Created by Jonathan Varghese on 5/9/24.
+//
+
+import Foundation
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"   
+    ]
+}
