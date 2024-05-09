a69c07a253481cf9b375351fa51c7473b9c27684
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-05-09 10:51:09 -0500

    Initial Commit

---
+++ CountryFlagGame2/CountryFlagGame2App.swift
@@ -0,0 +1,17 @@
+//
+//  CountryFlagGame2App.swift
+//  CountryFlagGame2
+//
+//  Created by Jonathan Varghese on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CountryFlagGame2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
